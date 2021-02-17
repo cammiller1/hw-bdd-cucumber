f030abeced4009fc960dade1d7ac631265e8517d
Cameron Miller <cm3959@columbia.edu>
2021-02-17 00:04:56 +0000

    tues 7pm

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,13 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    
+    #---- added ----#
+    #add each movie into the movie object
+    Movie.create!(movie)
+    #---------------#
   end
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +23,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +34,28 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+
+  #---- added ----#
+  log("***** rating_list #{rating_list}")
+  log("***** uncheck #{uncheck}")
+    
+  tokens = rating_list.split(', ')
+    
+  tokens.each do |field|
+      log("******#{field}******")
+      
+      if uncheck == ""
+          check(field)
+      elsif uncheck == "un"
+          uncheck(field)
+      end
+  end
+  #---------------#
+  
+#   fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
